7dad1030785bd86906129e8d671a504462004ec3
Carlos Marfil <carlos.marfil.r@gmail.com>
2022-10-13 12:39:29 +0100

    solved the distace kata

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,7 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  it("returns string with estimated time of arrival", () => {});
+  expect(reachDestination(44, 10)).toEqual("4.5");
+  expect(reachDestination(38, 4)).toEqual("9.5");
 });
