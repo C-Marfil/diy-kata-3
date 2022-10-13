0f092eba6fffe4f274d5fdcdc878201c9df0832f
Carlos Marfil <carlos.marfil.r@gmail.com>
2022-10-13 12:26:50 +0100

    Solved the kata with the dogs and cats lol

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,22 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (human) => {
+  let n1 = 0;
+  let n2 = 0;
+  const cat = n1;
+  const dog = n2;
+
+  if (human > 1) {
+    n1 = +15;
+    n2 = +15;
+  }
+  if (human >= 2) {
+    n1 = +15 + 9;
+    n2 = +15 + 9;
+  }
+  if (human >= 3) {
+    n1 = +15 + 9 + (4 * (human - 2));
+    n2 = +15 + 9 + (5 * (human - 2));
+  }
+  return [human, cat, dog];
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,14 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe('HumanCatDogYears', () => {
+  it("human years to cats and dogs equal their actual years 1", () => {
+    expect(humanCatDogYears(1)).toEqual[1, 15, 15];
+  });
+  it("human years to cats and dogs equal their actual years 2", () => {
+    expect(humanCatDogYears(2)).toEqual[2, 24, 24];
+  });
+  it("human years after 2 add 4 and 5 to cats and dogs respectively", () => {
+    expect(humanCatDogYears(10)).toEqual[10, 56, 64]
+  });
+  
+});
