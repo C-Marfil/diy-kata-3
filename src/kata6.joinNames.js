5179d1a2575be4c502d5b7bffd0f56e73fda9224
Carlos Marfil <carlos.marfil.r@gmail.com>
2022-10-13 13:12:08 +0100

    solved simpsons

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,17 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  let allTogether = [];
+  const basic = ", ";
+  const fancy = " & ";
+
+  for (let i = 0; i < namesObj.length; i += 1) {
+    allTogether += namesObj[i].name;
+    if (i === namesObj.length -2) {
+        allTogether += fancy;
+    } else if (i !== namesObj.length -2 && i !== namesObj.length -1) {
+        allTogether += basic;
+    }
+  }
+  return allTogether;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,39 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(
+      joinNames([
+        {
+          name: "Bart",
+        },
+        {
+          name: "Lisa",
+        },
+        {
+          name: "Maggie",
+        },
+      ])
+    ).toBe("Bart, Lisa & Maggie");
+  });
+  expect(
+    joinNames([
+      {
+        name: "Bart",
+      },
+      {
+        name: "Lisa",
+      },
+      {
+        name: "Maggie",
+      },
+      {
+        name: "Marge",
+      },
+      {
+        name: "Homer",
+      },
+    ])
+  ).toBe("Bart, Lisa, Maggie, Marge & Homer");
 });
+
